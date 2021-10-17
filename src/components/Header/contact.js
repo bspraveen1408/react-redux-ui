4e1f77c131498b3fd577f05282d456aeb495df11
B S Praveen <bsp.civil@gmail.com>
2021-10-17 12:41:28 +0530

    contact page is added

---
+++ src/components/Header/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+export default class Contacts extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h5>{this.props.info}</h5>
+            </div>
+        )
+    }
+}
