f74311364346364bcd178af473efd4edac4c7478
B S Praveen <bsp.civil@gmail.com>
2021-10-21 16:56:47 +0530

    Added header and footer for the app

--- src/components/Header/about.js
+++ src/components/Header/about.js
@@ -1,16 +1,9 @@
 import React from 'react';
-class About extends React.Component{
-    constructor(props) {
-        super(props)
-        this.state={
-
-        }
-
-    }
-    render(){
-        return(
-            <div>{this.props.details}
-            <h5>Soon this page will be working</h5>
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h5>Soon this page will be working</h5>
             </div>
         )
     }
--- src/components/Header/contact.js
+++ src/components/Header/contact.js
@@ -1,16 +1,10 @@
 import React from 'react';
-export default class Contacts extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-
-        }
-    }
-    render(){
-        return(
+export default class Contacts extends React.Component {
+    render() {
+        return (
             <div>
-                <h5>{this.props.info}</h5>
-                <br/>
+                <h5>Contact info not yet uploaded</h5>
+                <br />
             </div>
         )
     }
